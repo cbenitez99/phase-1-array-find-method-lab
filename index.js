396ec8041b2e4c0d7be9dc8c30d9be18fb501e70
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-06-14 14:52:33 -0500

    commit tree 6a28cb001783b5c25874c38482bfbee5cfae2cd8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record){
+    const result = record.find(record =>record.result === 'W');
+    if (!!result == true){
+        return result.year
+    }else{
+        return undefined;
+    }
+}
